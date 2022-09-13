d00c2614d4079c2c2f60f34292d49362565ed5eb
MatChumski <mateo0704@hotmail.com>
2022-09-12 22:25:28 -0500

    UI

--- FlappyThing/Assets/Scripts/scriptBird.cs
+++ FlappyThing/Assets/Scripts/scriptBird.cs
@@ -5,44 +5,93 @@ using UnityEngine;
 public class scriptBird : MonoBehaviour
 {
     
-    [SerializeField]
-    private int jumpForce = 6;
+    [SerializeField] private int jumpForce = 6;
 
-    Rigidbody2D playerPhysics;
+    private Rigidbody2D playerPhysics;
+
+    private SpriteRenderer playerSprite;
+
+    [SerializeField] private Sprite bird;
+    [SerializeField] private Sprite birdJump;
+
+    public AudioSource birdSound;
+    private AudioSource brdSnd;
+    public AudioClip jumpSound;
+
     public GameObject gameHandler;
+    private GameObject handler;
+    private scriptGameHandler scriptHandler;
+
+    float timer;
 
     // Start is called before the first frame update
     void Start()
     {
-        playerPhysics = GetComponent<Rigidbody2D>();  
-        
-        GameObject handler = Instantiate(gameHandler);
+        playerPhysics = GetComponent<Rigidbody2D>();
+        playerSprite = GetComponent<SpriteRenderer>();
+
+        brdSnd = Instantiate(birdSound);
+
+        brdSnd.enabled = true;
+
+        handler = Instantiate(gameHandler);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        scriptHandler = handler.GetComponent<scriptGameHandler>();
+
+        if (scriptHandler.status == "menu" && transform.position.y < 0 && timer > 0.5f) 
         {
-            float compensation = 0;
-            if (playerPhysics.velocity.y < 0)
-            {
-                compensation = playerPhysics.velocity.y * -1;
-            }
-            playerPhysics.AddForce(Vector2.up * (jumpForce + compensation), ForceMode2D.Impulse);
+            Jump();
+            timer = 0;
+        } else
+        {
+            timer += Time.deltaTime;
         }
+
+        if (scriptHandler.status == "play")
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Jump();
+            }
+        }
+
+        if (playerPhysics.velocity.y > 0)
+        {
+            playerSprite.sprite = birdJump;
+        }
+        else
+        {
+            playerSprite.sprite = bird;
+        }
+    }
+
+    void Jump()
+    {
+        float compensation = 0;
+        if (playerPhysics.velocity.y < 0)
+        {
+            compensation = playerPhysics.velocity.y * -1;
+        }
+        playerPhysics.AddForce(Vector2.up * (jumpForce + compensation), ForceMode2D.Impulse);
+
+        brdSnd.clip = jumpSound;
+        brdSnd.Play();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Wall")
         {
-            //Destroy(transform.root.gameObject);
+            scriptHandler.GameOver(gameObject);
         }
 
         if (collision.tag == "Score")
         {
-            Debug.Log("score");
+            scriptHandler.updateScore();
         }
     }
 }
--- FlappyThing/Assets/Scripts/scriptGameHandler.cs
+++ FlappyThing/Assets/Scripts/scriptGameHandler.cs
@@ -1,31 +1,139 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class scriptGameHandler : MonoBehaviour
 {
 
     public float speed;
+    public string status;
 
     public GameObject wall;
+
+    public AudioSource deathSoundImp;
+    private AudioSource deathSound;
+    public AudioClip deathClip;
+
+    // UI Imports
+    public Canvas canvasImp;
+    public TMP_Text textScoreImp;
+
+    public TMP_Text menuTitleImp;
+    public RectTransform menuPanelImp;
+    public Button menuButtonImp;
+
+    public GameObject gameOverPanelsImp;
+    public Button gameOverButtonImp;
+
+    // UI
+    private Canvas canvas;
+
+    // Score
+    private TMP_Text textScore;
+    public float score;
+
+    // Menu
+    private TMP_Text menuTitle;
+    private RectTransform menuPanel;
+    private Button menuButton;
+
+    // Game Over
+    private GameObject gameOverPanels;
+    public Button gameOverButton;
+
+    float wallTimer;
 
     // Start is called before the first frame update
     void Start()
     {
-        //float genSpeed = 1 + Mathf.Sqrt(speed);
-        //Debug.Log(genSpeed);
+        score = 0;
+        status = "menu";
 
-        InvokeRepeating("CreateWall", 0, 1f);
+        menuTitle = Instantiate(menuTitleImp);
+        menuPanel = Instantiate(menuPanelImp);
+        menuButton = Instantiate(menuButtonImp);
+
+        canvas = Instantiate(canvasImp);
+
+        menuPanel.transform.SetParent(canvas.transform, false);
+        menuTitle.transform.SetParent(canvas.transform, false);
+        menuButton.transform.SetParent(canvas.transform, false);
+
+        menuButton.onClick.AddListener(GameStart);
+
+        deathSound = Instantiate(deathSoundImp);
+        deathSound.enabled = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (status == "play")
+        {
+            if (wallTimer >= 1f)
+            {
+                CreateWall();
+                wallTimer = 0;
+            }
+            else
+            {
+                wallTimer += Time.deltaTime;
+            }
+        }
+    }
+
+    public void GameStart()
+    {
+        status = "play";
+
+        foreach (Transform child in canvas.transform)
+        {
+            GameObject.Destroy(child.gameObject);
+        }
+
+        textScore = Instantiate(textScoreImp);
+
+        textScore.transform.SetParent(canvas.transform, false);
+    }
+    public void GameOver(GameObject bird)
+    {
+        if (status == "play")
+        {
+            status = "dead";
+
+            gameOverPanels = Instantiate(gameOverPanelsImp);
+            gameOverButton = Instantiate(gameOverButtonImp);
+
+            gameOverPanels.transform.SetParent(canvas.transform, false);
+            gameOverButton.transform.SetParent(canvas.transform, false);
+
+            gameOverButton.onClick.AddListener(RestartGame);
+
+            deathSound.clip = deathClip;
+            deathSound.Play();
+
+            Destroy(bird);
+        }
+    }
+
+    private void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void updateScore()
+    {
+        score++;
+        textScore.text = score.ToString();
     }
 
     private void CreateWall()
     {
         GameObject newWall = Instantiate(wall);
     }
+
+
 }
